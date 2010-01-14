2a83f31eb5d582df27142d98a2b3cee31a65f861
versus <versus@versus-laptop.(none)>
2010-01-14 19:16:37 +0200

    add capistrano

---
+++ config/deploy.rb
@@ -0,0 +1,13 @@
+set :application, "nodeny"
+set :repository,  "git://github.com/versus/nodeny_www.git"
+set :scm, "nodeny.ukrindex.com"
+set :scm, :git
+set :branch, "master"
+set :ssh_options, { :forward_agent => true }
+set :use_sudo, false
+set :deploy_to, "~/#{application}"
+
+role :web, "nodeny.ukrindex.com"                          # Your HTTP server, Apache/etc
+role :app, "nodeny.ukrindex.com"                          # This may be the same as your `Web` server
+role :db,  "nodeny.ukrindex.com", :primary => true 
+
